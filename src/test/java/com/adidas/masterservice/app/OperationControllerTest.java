58f6622a53703b8f48901ffd9d90ce14359ffc87
avleichko <mrdezzdemon@gmail.com>
2019-05-06 00:56:50 +0200

    added spring restdock + spring mock mvc test

---
+++ src/test/java/com/adidas/masterservice/app/OperationControllerTest.java
@@ -0,0 +1,40 @@
+package com.adidas.masterservice.app;
+
+import com.adidas.masterservice.app.controllers.OperationController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.cloud.deployer.resource.support.DelegatingResourceLoader;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerProperties;
+import org.springframework.cloud.deployer.spi.local.LocalTaskLauncher;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@Import({LocalTaskLauncher.class,LocalDeployerProperties.class, DelegatingResourceLoader.class})
+@RunWith(SpringRunner.class)
+@WebMvcTest(OperationController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class OperationControllerTest{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/workerStart")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("started")))
+                .andDo(document("home"));
+    }
+
+}
