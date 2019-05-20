6f8060d81e1ac1cd4cfe7acb73da25fe811d611f
avleichko <mrdezzdemon@gmail.com>
2019-05-21 00:18:05 +0200

    added json util

--- src/main/java/com/adidas/masterservice/app/util/JsonUtil.java
+++ src/main/java/com/adidas/masterservice/app/util/JsonUtil.java
@@ -1,9 +1,14 @@
 package com.adidas.masterservice.app.util;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.hystrix.contrib.javanica.exception.CommandActionExecutionException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class JsonUtil {
@@ -14,5 +19,11 @@ public final class JsonUtil {
         } catch (Exception e) {
             throw new CommandActionExecutionException(e);
         }
+    }
+
+    //TODO needs to test it
+    public static <T> T convertJsonToPOJO(String jsonString, Class<?> target) throws JsonParseException, JsonMappingException, IOException, ClassNotFoundException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonString, objectMapper .getTypeFactory().constructCollectionType(List.class, Class.forName(target.getName())));
     }
 }
