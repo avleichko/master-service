76bcf2337fdc31de6ef27a9c576b56ad69012a95
avleichko <mrdezzdemon@gmail.com>
2019-05-21 00:12:05 +0200

    error metrics refactoring

--- src/main/java/com/adidas/masterservice/app/exceptions/ErrorControllerAdvice.java
+++ src/main/java/com/adidas/masterservice/app/exceptions/ErrorControllerAdvice.java
@@ -16,16 +16,8 @@ import java.util.Date;
 @Slf4j
 public class ErrorControllerAdvice {
 
-    private final MeterRegistry meterRegistry;
-
-    @Autowired
-    public ErrorControllerAdvice(MeterRegistry meterRegistry) {
-        this.meterRegistry = meterRegistry;
-    }
-
     @ExceptionHandler({ CommonMasterServiceException.class})                                                               // 400 HTTP code
     public ResponseEntity<ErrorDto> handleRuntimeException(Exception e) {
-        meterRegistry.counter("critical-errors", Tags.of("exception details", e.getMessage())).increment(1);
         final ErrorDto errorDto = new ErrorDto(new Date(), e.getMessage());
         return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
