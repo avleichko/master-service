ee6d9e83aeb9392e32f22d3f1bcdfefbd4da5b75
avleichko <mrdezzdemon@gmail.com>
2019-05-22 14:32:04 +0200

    added build script for dev porpose only

--- build.sh
+++ build.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-echo "build sttart"
+
+echo "available options are:"
+echo "* build"
+echo "* run"
+echo "* sonar"
+echo "* all"
+
+echo ""
+echo "passed arguments are:"
+echo $@
+
+if [[ $@ == *'build'* ]] || [[ $@ == *'all'* ]]
+then
+echo "build start"
 mvn clean install -DskipTests
 echo "build complete"
-sonar:sonar -Dsonar.projectKey=masterService -Dsonar.host.url=http://localhost:9000 -Dsonar.login=27af7a19bba21473c80c05044511e48e37734ebd
+fi
+
+if [[ $@ == *'sonar'* ]] || [[ $@ == *'all'* ]]
+then
+echo "sonar start"
+mvn sonar:sonar -Dsonar.projectKey=masterService -Dsonar.host.url=http://localhost:9000 -Dsonar.login=27af7a19bba21473c80c05044511e48e37734ebd
+echo "sonar end"
+fi
+
+if [[ $@ == *'run'* ]] || [[ $@ == *'all'* ]]
+then
+echo "running "
+echo "ok"
+
+exec java -jar ./target/*.jar
+fi
